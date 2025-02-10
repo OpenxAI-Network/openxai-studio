6b486d078a6df5693529eb2a258921a4f3b082b5
johnforfar <46335740+johnforfar@users.noreply.github.com>
2025-02-10 16:18:56 +0800

    logo update

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -2,10 +2,8 @@
 
 import Image from 'next/image'
 import Link from 'next/link'
-import discord from '@/assets/discord.svg'
-import linkedin from '@/assets/linkedin.svg'
 import twitter from '@/assets/twitter.svg'
-import logo from '@/public/images/old-logo.png'
+import logo from '@/public/images/openxai-logo.png'
 
 export default function Footer() {
   const footerItems = [
